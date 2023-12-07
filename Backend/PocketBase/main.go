78d56665d562d2bcacb7db3163466f383ac0364e
Patrick Buck <pappybuck@hotmail.com>
2023-12-07 13:39:53 -0500

    Updated Balance and Transactions

--- Backend/PocketBase/main.go
+++ Backend/PocketBase/main.go
@@ -8,11 +8,9 @@ import (
 	"os"
 	"strings"
 
-	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
-	"github.com/pocketbase/pocketbase/forms"
 	"github.com/pocketbase/pocketbase/models"
 	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 	"github.com/pocketbase/pocketbase/tools/cron"
@@ -34,7 +32,7 @@ func main() {
 		Use: "init",
 		Run: func(cmd *cobra.Command, args []string) {
 			startup(app)
-			UpdateDashboard(app)
+			updateDashboard(app)
 		},
 	})
 
@@ -43,7 +41,7 @@ func main() {
 
 		scheduler.MustAdd("UpdateDashboard", "*/10 * * * *", func() {
 			log.Println("UpdateDashboard")
-			UpdateDashboard(app)
+			updateDashboard(app)
 
 		})
 
@@ -53,50 +51,33 @@ func main() {
 
 	})
 
-	app.OnRecordAfterCreateRequest("users").Add(func(e *core.RecordCreateEvent) error {
-		collection, err := app.Dao().FindCollectionByNameOrId("Balances")
-		if err != nil {
-			log.Println(err)
-		}
-		record := models.NewRecord(collection)
-
-		form := forms.NewRecordUpsert(app, record)
-
-		form.LoadData(map[string]any{
-			"User":    e.Record.Id,
-			"Balance": 0,
-		})
-
-		if err := form.Submit(); err != nil {
-			log.Println(err)
-		}
-
-		return nil
-
-	})
-
 	app.OnRecordBeforeCreateRequest("Transactions").Add(func(e *core.RecordCreateEvent) error {
 
 		user := e.Record.Get("User").(string)
 
-		records, err := app.Dao().FindRecordsByExpr("Balances", dbx.HashExp{"User": user})
+		record, err := app.Dao().FindRecordById("users", user)
 		if err != nil {
 			log.Println(err)
 		}
 
-		for _, record := range records {
-			switch e.Record.Get("Type") {
-			case "Deposit":
-				record.Set("Balance", record.Get("Balance").(float64)+e.Record.Get("Amount").(float64))
-			case "Withdraw":
-				if record.Get("Balance").(float64) < e.Record.Get("Amount").(float64) {
-					return apis.NewBadRequestError("Insufficient funds", nil)
-				} else {
-					record.Set("Balance", record.Get("Balance").(float64)-e.Record.Get("Amount").(float64))
-				}
+		switch e.Record.Get("Type") {
+		case "Deposit":
+			record.Set("balance", record.Get("balance").(float64)+e.Record.Get("Amount").(float64))
+		case "Withdraw":
+			if record.Get("balance").(float64) < e.Record.Get("Amount").(float64) {
+				return apis.NewBadRequestError("Insufficient funds", nil)
+			} else {
+				record.Set("balance", record.Get("balance").(float64)-e.Record.Get("Amount").(float64))
 			}
-			app.Dao().Save(record)
+		case "Buy":
+			if record.Get("balance").(float64) < e.Record.Get("Amount").(float64) {
+				return apis.NewBadRequestError("Insufficient funds", nil)
+			} else {
+				record.Set("balance", record.Get("balance").(float64)-e.Record.Get("Amount").(float64))
+			}
+
 		}
+		app.Dao().Save(record)
 
 		return nil
 	})
@@ -107,7 +88,7 @@ func main() {
 
 }
 
-func UpdateDashboard(app *pocketbase.PocketBase) {
+func updateDashboard(app *pocketbase.PocketBase) {
 	collection, err := app.Dao().FindCollectionByNameOrId("DashboardCoins")
 	if err != nil {
 		log.Println(err)
