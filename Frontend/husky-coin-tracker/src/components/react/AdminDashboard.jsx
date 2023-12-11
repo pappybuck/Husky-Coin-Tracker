aca394950a825472ae036c3d0b29f27c2058df0a
Harry Allex <allexharry5@gmail.com>
2023-12-10 19:37:15 -0500

    admin css

--- Frontend/husky-coin-tracker/src/components/react/AdminDashboard.jsx
+++ Frontend/husky-coin-tracker/src/components/react/AdminDashboard.jsx
@@ -1,14 +1,14 @@
 import StatisticBox from "./StatisticBox";
 import UserList from "./UserList";
 
-function AdminDashboard({users, coins}) {
+function AdminDashboard({users, coins, transactions}) {
     return (
     <div>
-        <p className ="ms-12 mt-10 font-bold text-xl"> Admin Dashboard </p>
+        <p className ="ms-12 mt-10 text-4xl font-bold text-gray-700"> Admin Dashboard </p>
         <div className="grid grid-rows-1 grid-flow-col mx-60 my-24">
             <StatisticBox title={'Users'} statistic={users.length}/>
-            <StatisticBox title={'Transactions'} statistic={'300'}/>
             <StatisticBox title={'Coins'} statistic={coins.length}/>
+            <StatisticBox title={'Transactions'} statistic={transactions}/>
         </div>
         <UserList users={users}/>
     </div>)
