9a12e73e220cb25924365da7d7feab8cfa8747b4
Harry Allex <allexharry5@gmail.com>
2023-12-13 22:53:39 -0500

    admin toggle

--- Frontend/husky-coin-tracker/src/components/react/UserList.jsx
+++ Frontend/husky-coin-tracker/src/components/react/UserList.jsx
@@ -69,18 +69,26 @@ function UserList({ users }) {
                                     {user.created}
                                 </td>
                                 <td className="px-6 py-3">
-       
-                                    {!user.isAdmin &&
-                                    <form
+                                <form
                                     method="POST"
-                                    className="space-y-8 mb-4"
                                     acceptCharset="UTF-8"
+                                    className="ms-4"
                                     encType="multipart/form-data"
-                                    ><button type="submit" name="id" value={user.id} className="bg-red-600 hover:bg-red-700 text-white py-2 px-20 rounded">Set Admin</button></form>}
-                                    {user.isAdmin && <div><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                                            </svg>
-                                                            </div>}
+                                    >
+                                    {!user.isAdmin &&
+                                    <button type="submit" name="id" value={user.id}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" color="red" className="w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        </svg>
+
+                                    </button>}
+                                    {user.isAdmin && 
+                                        <button type="submit" name="id" value={user.id}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6" color="#22c55e">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                            </svg>
+                                        </button>}
+                                </form>
                                 </td>
                                 <td className="px-6 py-3 flex justify-end">
                                     <a href={"/profile/" + user.id} className="bg-red-600 hover:bg-red-700 text-white py-2 px-20 rounded">
